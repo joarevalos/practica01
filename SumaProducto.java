78a2b923b6fdf96d1fca06248998d3e069dc11ad
joarevalos <joarevalos@gmail.com>
2023-12-12 19:22:27 -0300

    Uso de colores

--- SumaProducto.java
+++ SumaProducto.java
@@ -3,6 +3,16 @@
 import java.util.Scanner;
 
 public class SumaProducto {
+    static String black = "\033[30m";
+    static String red = "\033[31m";
+    static String green = "\033[32m";
+    static String yellow = "\033[33m";
+    static String blue = "\033[34m";
+    static String purple = "\033[35m";
+    static String cyan = "\033[36m";
+    static String white = "\033[37m";
+    static String reset = "\u001B[0m";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int num1, num2, suma, producto;
@@ -12,8 +22,9 @@ public class SumaProducto {
         num2 = scanner.nextInt();
         suma = num1 + num2;
         producto = num1 * num2;
-        System.out.println("Resultado la suma es " + suma);
-        System.out.println("Resultado la multiplicacion es " + producto);
+        System.out.println(green + "Resultado la suma es " + suma);
+        System.out.println(yellow + "Resultado la multiplicacion es " + producto);
         scanner.close();
+        System.out.println(reset);
     }
 }
