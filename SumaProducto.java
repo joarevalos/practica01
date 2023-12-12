207d1596681dc08052f1937fc91929f8253752dd
joarevalos <joarevalos@gmail.com>
2023-12-12 16:24:45 -0300

    Tercero cambio

--- SumaProducto.java
+++ SumaProducto.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class SumaProducto {
     public static void main(String[] args) {
-        try (Scanner scan = new Scanner(System.in)) {
-            int num1, num2, suma, producto;
-            System.out.print("Ingrese primer numero: ");
-            num1 = scan.nextInt();
-            System.out.print("Ingrese segundo numero: ");
-            num2 = scan.nextInt();
-            suma = num1 + num2;
-            producto = num1 * num2;
-        }
+        Scanner scanner = new Scanner(System.in);
+        int num1, num2, suma, producto;
+        System.out.print("Ingrese primer numero: ");
+        num1 = scanner.nextInt();
+        System.out.print("Ingrese segundo numero: ");
+        num2 = scanner.nextInt();
+        suma = num1 + num2;
+        producto = num1 * num2;
         System.out.println("Resultado la suma es " + suma);
         System.out.println("Resultado la multiplicacion es " + producto);
+        scanner.close();
     }
 }
