5fddbb59bedf312f93a0fa82947acb8b1279ba02
joarevalos <joarevalos@gmail.com>
2023-12-12 16:14:47 -0300

    Prueba Contreol de Version Java

---
+++ HolaMundo.java
@@ -0,0 +1,13 @@
+//primer programa Hola mundo
+//Lenguaje de Programacion I
+public class HolaMundo {
+    // metodo constructor
+    public HolaMundo() {
+    }
+
+    // metodo principal
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("Mundo!");
+    }
+}
---
+++ SumaProducto.java
@@ -0,0 +1,18 @@
+
+//Programa que suma y multiplica numeros
+import java.util.Scanner;
+
+public class SumaProducto {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num1, num2, suma, producto;
+        System.out.print("Ingrese primer numero: ");
+        num1 = scan.nextInt();
+        System.out.print("Ingrese segundo numero: ");
+        num2 = scan.nextInt();
+        suma = num1 + num2;
+        producto = num1 * num2;
+        System.out.println("Resultado la suma es " + suma);
+        System.out.println("Resultado la multiplicacion es " + producto);
+    }
+}
